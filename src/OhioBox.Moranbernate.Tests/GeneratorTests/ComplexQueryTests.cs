921b8339ad672cd1f6e403396616ecaf8bf3728b
ysa23 <ysa.angel@gmail.com>
2018-05-21 12:49:35 +0300

    Delete redundant using

--- src/OhioBox.Moranbernate.Tests/GeneratorTests/ComplexQueryTests.cs
+++ src/OhioBox.Moranbernate.Tests/GeneratorTests/ComplexQueryTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
 using OhioBox.Moranbernate.Querying;
 using OhioBox.Moranbernate.Tests.Domain;
 using NUnit.Framework;
