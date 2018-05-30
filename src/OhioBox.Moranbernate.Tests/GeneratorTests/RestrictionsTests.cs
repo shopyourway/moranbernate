a5174d64060a10b033d8c4b0f319d1da086e54da
ysa23 <ysa.angel@gmail.com>
2018-05-30 17:17:12 +0300

    Add regex restriction

--- src/OhioBox.Moranbernate.Tests/GeneratorTests/RestrictionsTests.cs
+++ src/OhioBox.Moranbernate.Tests/GeneratorTests/RestrictionsTests.cs
@@ -38,5 +38,19 @@ namespace OhioBox.Moranbernate.Tests.GeneratorTests
 			Assert.AreEqual("`LongColumnName` = ?p0", sql);
 			Assert.AreEqual(5, parameters[0]);
 		}
+
+		[Test]
+		public void Regex()
+		{
+			var map = MappingRepo<SimpleObject>.GetMap();
+			var property = ExpressionProcessor<SimpleObject>.GetPropertyFromCache(x => x.Long);
+
+			var res = new RegexRestriction<string>(property, "[a-zA-Z]+");
+			var parameters = new List<object>();
+			var sql = res.Apply(parameters, map.Dialect);
+
+			Assert.AreEqual("`LongColumnName` REGEX ?p0", sql);
+			Assert.AreEqual("[a-zA-Z]+", parameters[0]);
+		}
 	}
 }
---
+++ src/OhioBox.Moranbernate/Querying/Restrictions/RegexRestriction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using OhioBox.Moranbernate.Mapping;
+
+namespace OhioBox.Moranbernate.Querying.Restrictions
+{
+	internal class RegexRestriction<T> : IRestriction
+	{
+		private readonly Property _property;
+		private readonly T _value;
+
+		public RegexRestriction(Property property, T value)
+		{
+			_property = property;
+			_value = value;
+		}
+
+		public string Apply(List<object> parameters, IDialect dialect)
+		{
+			var sql = dialect.RegexMatch(_property.ColumnName, dialect.CreateParameter("p" + parameters.Count));
+			parameters.Add(_property.ToParameter(_value));
+			return sql;
+		}
+	}
+}
